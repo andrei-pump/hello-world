2b1f484b0bbc0e84159e447feae546d95c6328f6
Andrei Dragnea <andrei@pump.fun>
2024-06-18 14:27:09 +0300

    Emit event

--- programs/hello-world/src/lib.rs
+++ programs/hello-world/src/lib.rs
@@ -5,11 +5,22 @@ declare_id!("6WWEZXoUdgrJ9AkbLdLBfZDKWVAXvEna8fRY8yrvvCNE");
 #[program]
 pub mod hello_world {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn hello_world(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("Hello world, from solana smart contract");
+        emit!(MyEvent {
+            data: 5,
+            label: "hello".to_string(),
+        });
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[event]
+pub struct MyEvent {
+    pub data: u64,
+    #[index]
+    pub label: String,
+}
--- tests/hello-world.ts
+++ tests/hello-world.ts
@@ -3,14 +3,11 @@ import { Program } from "@coral-xyz/anchor";
 import { HelloWorld } from "../target/types/hello_world";
 
 describe("hello-world", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Mic testing - Hello world", async () => {
+    const tx = await program.methods.helloWorld().rpc();
     console.log("Your transaction signature", tx);
   });
 });
