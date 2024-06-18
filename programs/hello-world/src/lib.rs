0ac30b450cfc270b97aa2e461e8ddcecebcd7b71
Andrei Dragnea <andrei@pump.fun>
2024-06-18 14:45:45 +0300

    Emit CPI event

--- programs/hello-world/src/lib.rs
+++ programs/hello-world/src/lib.rs
@@ -5,16 +5,21 @@ declare_id!("6WWEZXoUdgrJ9AkbLdLBfZDKWVAXvEna8fRY8yrvvCNE");
 #[program]
 pub mod hello_world {
     use super::*;
-    pub fn hello_world(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn hello_world(ctx: Context<Initialize>) -> Result<()> {
         msg!("Hello world, from solana smart contract");
         emit!(MyEvent {
             data: 5,
             label: "hello".to_string(),
         });
+        emit_cpi!(MyEvent {
+            data: 7,
+            label: "cpi".to_string(),
+        });
         Ok(())
     }
 }
 
+#[event_cpi]
 #[derive(Accounts)]
 pub struct Initialize {}
 
